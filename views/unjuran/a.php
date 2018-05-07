3d2bfe81bf6a5647339bab5e39a5ed1908063ee2
CIAST <ciast@CIASTs-MacBook-Pro-4.local>
2018-05-07 08:43:58 +0800

    kodA

--- views/unjuran/a.php
+++ views/unjuran/a.php
@@ -59,21 +59,41 @@ use app\models\Unjuran;
 		<tr><th>Jumlah Unjuran</th><td><?= number_format($model->jumlah_unjuran, 2) ?></td></tr>
 		<tr><th>Catatan</th><td><?= $model->catatan ?></td></tr>
         <tr><th>Jumlah Baki Unjuran yang ada </th><td><?= number_format($jumlah_agihan - $jumlah_ujuran_A, 2); ?></td></tr>
+        <tr><th>Kelulusan </th><td>
+                                <input name="lulus" type="radio" checked value="0"> Tukar kepada kod A seperti nilai unjuran. <br>
+                                <input name="lulus" type="radio" value="1"> Tukar kepada kod A dengan perubahan.
+                               </td>
+        </tr>
+        
 	</tbody>
 </table>
 
-<div class="jabatan-form">
+<div class="jabatan-form" style="display: none;">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'jabatan')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'jabatan')->hiddenInput(['maxlength' => true])->label(false) ?>
 
     <?= $form->field($model, 'jumlah_unjuran')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Simpan'), ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton(Yii::t('app', 'Simpan'), ['class' => 'btn btn-success', 'data-confirm' => 'Simpan?']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
 
 </div>	
+
+<?php
+
+$this->registerJs('
+    $("input[name=lulus]").click(function(){
+        if($(this).val() == "1") {
+            $(".jabatan-form").eq(0).show(300);
+        }
+        else
+            $(".jabatan-form").eq(0).hide(300);
+    });
+');
+
+?>
